ca25714a189af880178c5fae80738e778d528248
surajkaushik <kaushiksuraj1102@gmail.com>
2022-11-15 00:10:24 +0530

    first commit

---
+++ digitalbook/digitalbook/src/main/java/com/iiht/digitalbook/DigitalbookApplication.java
@@ -0,0 +1,13 @@
+package com.iiht.digitalbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalbookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalbookApplication.class, args);
+	}
+
+}
