61cdc9c3b34d1ba0f59a72e8132ee85ca2f47879
surajkaushik <kaushiksuraj1102@gmail.com>
2022-11-17 14:52:45 +0530

    Added Student Entity

--- StudentManagmentSystem/src/main/java/com/example/demo/entity/Student.java
+++ StudentManagmentSystem/src/main/java/com/example/demo/entity/Student.java
@@ -1,5 +1,52 @@
 package com.example.demo.entity;
 
+import javax.persistence.Entity;
+
+@Entity
 public class Student {
+	
+	private Integer sId;
+	private String sName;
+	private String sEmail;
+	private Integer standard;
+	
+	public Student() {
+		super();
+	}
+	
+	public Student(Integer sId, String sName, String sEmail, Integer standard) {
+		super();
+		this.sId = sId;
+		this.sName = sName;
+		this.sEmail = sEmail;
+		this.standard = standard;
+	}
+	
+	public Integer getsId() {
+		return sId;
+	}
+	public void setsId(Integer sId) {
+		this.sId = sId;
+	}
+	public String getsName() {
+		return sName;
+	}
+	public void setsName(String sName) {
+		this.sName = sName;
+	}
+	public String getsEmail() {
+		return sEmail;
+	}
+	public void setsEmail(String sEmail) {
+		this.sEmail = sEmail;
+	}
+	public Integer getStandard() {
+		return standard;
+	}
+	public void setStandard(Integer standard) {
+		this.standard = standard;
+	}
+	
+	
 
 }
