cdcb413655a78b8750df538082eb51d6cd661ce6
surajkaushik <kaushiksuraj1102@gmail.com>
2022-11-17 14:42:43 +0530

    Craeted structure for StudentManagmemntSystem

--- StudentManagmentSystem/src/main/java/com/example/demo/StudentManagmentSystemApplication.java
+++ StudentManagmentSystem/src/main/java/com/example/demo/StudentManagmentSystemApplication.java
@@ -2,19 +2,11 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+
 
 @SpringBootApplication
-@RestController
 public class StudentManagmentSystemApplication {
 	
-	@GetMapping("/hello/{name}")
-	public String Helloworld(@PathVariable String name) {
-		return "Hey "+name+". Goood Morning";
-	}
-
 	public static void main(String[] args) {
 		SpringApplication.run(StudentManagmentSystemApplication.class, args);
 	}
